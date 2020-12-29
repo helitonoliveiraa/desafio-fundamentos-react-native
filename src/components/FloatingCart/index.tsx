2565272734f50a92d23039d7b34f7b3db620f298
Héliton Oliveira <heliton.oliveira88@gmail.com>
2020-12-29 11:18:01 -0300

    :sparkles: challenge done

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -25,14 +25,20 @@ const FloatingCart: React.FC = () => {
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      return accumulator + product.price * product.quantity;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItems = products.reduce((accumulator, product) => {
+      return accumulator + product.quantity;
+    }, 0);
 
-    return 0;
+    return totalItems;
   }, [products]);
 
   return (
